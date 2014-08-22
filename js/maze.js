2f69b877d9aec2b3c1f75450a39d52f4cf1bcd97
Jason Mannon <jmannonpdx@gmail.com>
2014-08-21 19:53:40 -0700

    added setwall function

--- js/maze.js
+++ js/maze.js
@@ -9,6 +9,16 @@ function Maze(width, height) {
   this.startOrientation = null;
   this.endX = null;
   this.endY = null;
+
+  this.spaces = [];
+
+  var x, y;
+  for (x=1; x <= width; x += 1) {
+    this.spaces[x] = [];
+    for (y=1; y <= height; y += 1) {
+      this.spaces[x][y] = new MazeSpace();
+    }
+  }
 }
 
 Maze.prototype.setStart = function(x, y, orientation) {
@@ -17,7 +27,11 @@ Maze.prototype.setStart = function(x, y, orientation) {
   this.startOrientation = orientation;
 }
 
-Maze.prototype..setExit = function(x, y) {
+Maze.prototype.setExit = function(x, y) {
   this.endX = x;
   this.endY = y;
+}
+
+Maze.prototype.setWall = function(x, y, direction) {
+  this.spaces[x][y].setWall(direction);
 }
